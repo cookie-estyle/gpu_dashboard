b56b94cd5672b586beb61db7815354abc2c6e6e2
cookie-estyle <k.kurosawa@estyle-inc.jp>
2024-10-30 17:29:05 +0900

    refactor: uploader内の不要なimportを削除

--- src/uploader/data_processor.py
+++ src/uploader/data_processor.py
@@ -1,9 +1,4 @@
 import polars as pl
-import wandb
-import json
-from pathlib import Path
-from ..utils.config import CONFIG
-from ..uploader.artifact_handler import ArtifactHandler
 
 class DataProcessor:
     @staticmethod
@@ -27,7 +22,6 @@ class DataProcessor:
         try:
             new_runs_df = df.with_columns(
                 pl.col("run_id").cast(pl.Utf8),
-                #pl.col("assigned_gpu_node").cast(pl.Int64),
                 pl.col("duration_hour").cast(pl.Float64),
                 pl.col("gpu_count").cast(pl.Int64),
                 pl.col("average_gpu_utilization").cast(pl.Float64),
